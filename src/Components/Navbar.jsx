cc9faf81fd094ee38c1586e2e2023977a88c97c5
Vishnu joshi <vishnuakj59@gmail.com>
2023-09-29 21:29:59 +0530

    sm changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,6 +33,15 @@ const Navbar = () => {
             <li className='p-4'>Contact</li>
 
             </ul>
+        {/* <ul className='uppercase p-4'>
+            <li className='p-4 border-b border-gray-600 '>Home</li>
+            <li className='p-4 border-b border-gray-600 '>Company</li>
+            <li className='p-4 border-b border-gray-600 '>Resources</li>
+            <li className='p-4 border-b border-gray-600 '>About</li>
+            <li className='p-4'>Contact</li>
+
+            </ul> */}
+            
     </div>
     
     </div>
